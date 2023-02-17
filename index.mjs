ecdb9e8efd7a145ea036468ddea376cbe9a555ae
1596944197 <1596944197@qq.com>
2023-02-17 17:26:33 +0800

    feat:接入sql测试

--- index.mjs
+++ index.mjs
@@ -10,18 +10,19 @@ const connection = Mysql.createConnection({
   password: "password",
 });
 
+let query = (sql) => 1
+
 connection.connect(async (err) => {
   if (err) {
     console.error(err);
     throw Error("连接失败");
   }
-  const query = MysqlQuery(connection);
+  query = MysqlQuery(connection);
 
   let res = await query("CREATE DATABASE IF NOT EXISTS Test");
 
   res = await query('use Test')
-  debugger
-  res = await query(`CREATE TABLE users (
+  res = await query(`CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -29,44 +30,51 @@ connection.connect(async (err) => {
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   )`);
-  debugger;
+
+  res = await query(`CREATE TABLE IF NOT EXISTS chatRecord (
+    UserID int DEFAULT NULL,
+    Message text,
+    TimeStamp datetime DEFAULT CURRENT_TIMESTAMP,
+    id int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`)
 });
 
-() => {
-  const GPTApi = new ChatGPTAPI({
-    apiKey: "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",
+const GPTApi = new ChatGPTAPI({
+  apiKey: "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",
+});
+const ws = new WebSocketServer({
+  port: 3332,
+});
+
+ws.on("error", console.error);
+ws.once("listening", () => console.log("ws正在监听"));
+
+/** @returns {每个 wss 都指向一个用户} */
+ws.on("connection", (wss) => {
+  let currentGpt;
+
+  wss.on("message", async (data) => {
+    try {
+      const options = currentGpt
+        ? {
+          conversationId: currentGpt.conversationId,
+          parentMessageId: currentGpt.id,
+        }
+        : {};
+
+      const response = await GPTApi.sendMessage(
+        JSON.parse(data.toString()),
+        options
+      );
+      currentGpt === response ? null : (currentGpt = response);
+
+      query(`INSERT INTO chatRecord (UserID, Message) VALUES('${Math.random()}', '${response.text}')`)
+
+      wss.send(response.text);
+    } catch (error) {
+      console.warn(error);
+      wss.send("sorry,this bot is wrong,please wait a few");
+    }
   });
-  const ws = new WebSocketServer({
-    port: 3332,
-  });
-
-  ws.on("error", console.error);
-  ws.once("listening", () => console.log("ws正在监听"));
-
-  /** @returns {每个 wss 都指向一个用户} */
-  ws.on("connection", (wss) => {
-    let currentGpt;
-
-    wss.on("message", async (data) => {
-      try {
-        const options = currentGpt
-          ? {
-            conversationId: currentGpt.conversationId,
-            parentMessageId: currentGpt.id,
-          }
-          : {};
-
-        const response = await GPTApi.sendMessage(
-          JSON.parse(data.toString()),
-          options
-        );
-        currentGpt === response ? null : (currentGpt = response);
-
-        wss.send(response.text);
-      } catch (error) {
-        console.warn(error);
-        wss.send("sorry,this bot is wrong,please wait a few");
-      }
-    });
-  });
-};
+});
